28ec1c5e488672e13bb654f7a258fa31cb0e09db
Diana <93685325+dyannary@users.noreply.github.com>
2023-05-30 16:47:07 +0300

    add new entities (#5)

--- Notes.DataTransferObjects/Notes/NoteDto.cs
+++ Notes.DataTransferObjects/Notes/NoteDto.cs
@@ -1,8 +1,10 @@
-﻿namespace Notes.DataTransferObjects.Notes;
+﻿using Notes.DataTransferObjects.Enums;
+
+namespace Notes.DataTransferObjects.Notes;
 
 public class NoteDto
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
 }
