3dbc19b59417ce67c59fabcf5d6361eb7c0c87b9
Sam R <crutcovschi@gmail.com>
2023-06-02 22:04:05 +0300

    add id field for dtos

--- Notes.DataTransferObjects/Events/EventDto.cs
+++ Notes.DataTransferObjects/Events/EventDto.cs
@@ -2,6 +2,7 @@
 
 public class EventDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime Date { get; set; }
