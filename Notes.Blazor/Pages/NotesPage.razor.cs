8f53ad336a64969e6bf15530a2347e5f3f11af65
Sam R <crutcovschi@gmail.com>
2023-06-02 21:46:04 +0300

    add note details page and note services

---
+++ Notes.Blazor/Pages/NotesPage.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Notes.Blazor.Services.Interfaces;
+using Notes.DataTransferObjects.Notes;
+
+namespace Notes.Blazor.Pages;
+
+public partial class NotesPage
+{
+    [Inject] 
+    public INoteService NoteService { get; set; }
+    
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    public IEnumerable<NoteDto>? Notes;
+    public string ErrorMessage { get; set; } = string.Empty;
+        
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            Notes = await NoteService.GetNotesAsync();
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = e.Message;
+        }
+    }
+
+    private void NewNote()
+    {
+        NavigationManager.NavigateTo("newNote");
+    }
+}
