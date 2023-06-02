f0f23cd24ddb6b079cc9ac6370e0b81872b08039
1Leomas <81852464+1Leomas@users.noreply.github.com>
2023-06-02 22:04:54 +0300

    add id field for dtos (#12)

--- Notes.DataTransferObjects/Notes/NoteDto.cs
+++ Notes.DataTransferObjects/Notes/NoteDto.cs
@@ -4,6 +4,7 @@ namespace Notes.DataTransferObjects.Notes;
 
 public class NoteDto
 {
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
 }
--- Notes.DataTransferObjects/Tasks/TaskDto.cs
+++ Notes.DataTransferObjects/Tasks/TaskDto.cs
@@ -4,6 +4,7 @@ namespace Notes.DataTransferObjects.Tasks;
 
 public class TaskDto
 {
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public Priority Priority { get; set; }
