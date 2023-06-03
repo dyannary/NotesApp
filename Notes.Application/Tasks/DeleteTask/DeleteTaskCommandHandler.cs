83f5b637097383363bdf6819af9d2aa713a87097
Diana <93685325+dyannary@users.noreply.github.com>
2023-06-03 10:16:38 +0300

    add status code validation to tasks controller (#16)

--- Notes.Application/Tasks/DeleteTask/DeleteTaskCommandHandler.cs
+++ Notes.Application/Tasks/DeleteTask/DeleteTaskCommandHandler.cs
@@ -4,7 +4,7 @@ using Notes.Persistence.Context;
 
 namespace Notes.Application.Tasks.DeleteTask;
 
-public class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand, Unit>
+public class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand, Unit?>
 {
     private readonly NotesDbContext _notesDbContext;
 
@@ -12,9 +12,14 @@ public class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand, Unit>
     {
         _notesDbContext = notesDbContext;
     }
-    public async Task<Unit> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+    public async Task<Unit?> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
     {
-        var taskToDelete = await _notesDbContext.Tasks.FirstAsync(x => x.Id == request.Id);
+        var taskToDelete = await _notesDbContext.Tasks.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (taskToDelete == null)
+        {
+            return null;
+        }
 
         _notesDbContext.Tasks.Remove(taskToDelete);
 
