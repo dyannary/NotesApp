ffc680b408db80e18a7694b0ade0af36900eb537
Diana <diana.ribac001@gmail.com>
2023-06-05 04:07:13 +0300

    add color field to Note entity

--- Notes.DataTransferObjects/Notes/AddEditNoteDto.cs
+++ Notes.DataTransferObjects/Notes/AddEditNoteDto.cs
@@ -8,4 +8,5 @@ public class AddEditNoteDto
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public DateTime CreatedDate { get; set; }
+    public string Color { get; set; } = string.Empty;
 }
--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -4,4 +4,5 @@ public class Note : Entity
 {
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
 }
