66c145e7f74a48708367711df7bd3c3c6a5217a4
Sam <crutcovschi@gmail.com>
2023-06-06 04:19:46 +0300

    start implement builder for creating note and implement prototype

---
+++ Notes.Blazor/Builder/NoteBuilder.cs
@@ -0,0 +1,56 @@
+﻿using Notes.DataTransferObjects.Notes;
+using Notes.DataTransferObjects.NoteTags;
+
+namespace Notes.Blazor.Builder;
+
+public class NoteBuilder
+{
+    private NoteDto _note = new();
+
+    public NoteBuilder()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _note = new NoteDto();
+    }
+
+    public NoteBuilder AddTitle(string title)
+    {
+        _note.Title = title;
+        return this;
+    }
+
+    public NoteBuilder AddContent(string content)
+    {
+        _note.Content = content;
+        return this;
+    }
+
+    public NoteBuilder AddBorderColor(string color)
+    {
+        _note.Color = color;
+        return this;
+    }
+
+    public NoteBuilder RemoveBorderColor(string color)
+    {
+        _note.Color = "#27272f";
+        return this;
+    }
+
+    public NoteBuilder AddTag(NoteTagDto noteTag)
+    {
+        _note.NoteTags.Add(noteTag);
+        return this;
+    }
+
+    public NoteDto Build()
+    {
+        var note = _note.Clone();
+        Reset();
+        return note;
+    }
+}
--- Notes.DataTransferObjects/Notes/NoteDto.cs
+++ Notes.DataTransferObjects/Notes/NoteDto.cs
@@ -10,4 +10,24 @@ public class NoteDto
     public DateTime CreatedDate { get; set; }
     public string Color { get; set; } = string.Empty;
     public List<NoteTagDto> NoteTags { get; set; } = new List<NoteTagDto>();
+
+
+    public NoteDto Clone()
+    {
+        var noteTags = new List<NoteTagDto>();
+        foreach (var noteTag in NoteTags)
+        {
+            noteTags.Add(noteTag);
+        }
+
+        return new NoteDto()
+        {
+            Id = Id,
+            Title = Title,
+            Content = Content,
+            CreatedDate = CreatedDate,
+            Color = Color,
+            NoteTags = noteTags
+        };
+    }
 }
