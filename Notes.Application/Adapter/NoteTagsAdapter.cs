94d91d97fedd03865aeb7b90fc6fd5a4acc00edd
Diana <diana.ribac001@gmail.com>
2023-06-06 13:04:07 +0300

    add adapter pattern to note tags

---
+++ Notes.Application/Adapter/NoteTagsAdapter.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Notes.DataTransferObjects.NoteTags;
+using Notes.Domain;
+
+namespace Notes.Application.Adapter;
+
+public class NoteTagsAdapter : ITypeConverter<NoteTagDto, NoteTag>, ITypeConverter<NoteTag, NoteTagDto>
+{
+    public NoteTag Convert(NoteTagDto source, NoteTag destination, ResolutionContext context)
+    {
+        var noteTag = new NoteTag
+        {
+            Id = source.Id,
+            NoteId = source.NoteId,
+            Title = source.Title,
+            CreatedDate = source.CreatedDate,
+            Color = source.Color
+        };
+
+        return noteTag;
+    }
+
+    public NoteTagDto Convert(NoteTag source, NoteTagDto destination, ResolutionContext context)
+    {
+        var noteTagDto = new NoteTagDto
+        {
+            Id = source.Id,
+            NoteId = source.NoteId,
+            Title = source.Title,
+            CreatedDate = source.CreatedDate,
+            Color = source.Color
+        };
+
+        return noteTagDto;
+    }
+}
