56d989312d1c349ed805b41133566c0ee535dccf
Diana <diana.ribac001@gmail.com>
2023-06-06 07:41:53 +0300

    CONFIGS
    add adapter pattern for notes mapping

---
+++ Notes.Application/Adapter/NoteAdapter.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Notes.DataTransferObjects.Notes;
+using Notes.Domain;
+
+namespace Notes.Application.Adapter;
+
+public class NoteAdapter : ITypeConverter<AddEditNoteDto, Note>, ITypeConverter<Note, NoteDto>
+{
+    public Note Convert(AddEditNoteDto source, Note destination, ResolutionContext context)
+    {
+        var note = new Note
+        {
+            Id = source.Id,
+            Title = source.Title,
+            Content = source.Content,
+            CreatedDate = source.CreatedDate,
+            Color = source.Color
+        };
+
+        return note;
+    }
+
+    public NoteDto Convert(Note source, NoteDto destination, ResolutionContext context)
+    {
+        var noteDto = new NoteDto
+        {
+            Id = source.Id,
+            Title = source.Title,
+            Content = source.Content,
+            CreatedDate = source.CreatedDate,
+            Color = source.Color
+        };
+
+        return noteDto;
+    }
+}
