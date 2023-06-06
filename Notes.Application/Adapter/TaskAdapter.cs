19a107f356959fca863c3c01c52a11feb027c31a
Diana <diana.ribac001@gmail.com>
2023-06-06 07:52:43 +0300

    add adapter pattern for tasks and events mapping

---
+++ Notes.Application/Adapter/TaskAdapter.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Notes.DataTransferObjects.Tasks;
+
+namespace Notes.Application.Adapter;
+
+public class TaskAdapter : ITypeConverter<AddEditTaskDto, Domain.Task>, ITypeConverter<Domain.Task, TaskDto>
+{
+    public Domain.Task Convert(AddEditTaskDto source, Domain.Task destination, ResolutionContext context)
+    {
+        var task = new Domain.Task
+        {
+            Id = source.Id,
+            Title = source.Title,
+            Content = source.Content,
+            Priority = source.Priority,
+            Deadline = source.Deadline,
+            CreatedDate = source.CreatedDate,
+            IsCompleted = source.IsCompleted
+        };
+
+        return task;
+    }
+
+    public TaskDto Convert(Domain.Task source, TaskDto destination, ResolutionContext context)
+    {
+        var taskDto = new TaskDto
+        {
+            Id = source.Id,
+            Title = source.Title,
+            Content = source.Content,
+            Priority = source.Priority,
+            Deadline = source.Deadline,
+            CreatedDate = source.CreatedDate,
+            IsCompleted = source.IsCompleted
+        };
+
+        return taskDto;
+    }
+}
--- Notes.Application/Tasks/TaskMappingProfile.cs
+++ Notes.Application/Tasks/TaskMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Notes.Application.Adapter;
 using Notes.DataTransferObjects.Tasks;
 namespace Notes.Application.Tasks;
 
@@ -6,22 +7,7 @@ public class TaskMappingProfile : Profile
 {
     public TaskMappingProfile()
     {
-        CreateMap<AddEditTaskDto, Domain.Task>()
-            .ForMember(x => x.Id, opts => opts.MapFrom(op => op.Id))
-            .ForMember(x => x.Title, opts => opts.MapFrom(op => op.Title))
-            .ForMember(x => x.Content, opts => opts.MapFrom(op => op.Content))
-            .ForMember(x => x.Priority, opts => opts.MapFrom(op => op.Priority))
-            .ForMember(x => x.Deadline, opts => opts.MapFrom(op => op.Deadline))
-            .ForMember(x => x.CreatedDate, opts => opts.MapFrom(op => op.CreatedDate))
-            .ForMember(x => x.IsCompleted, opts => opts.MapFrom(op => op.IsCompleted));
-
-        CreateMap<Domain.Task, TaskDto>()
-            .ForMember(x => x.Id, opts => opts.MapFrom(op => op.Id))
-            .ForMember(x => x.Title, opts => opts.MapFrom(op => op.Title))
-            .ForMember(x => x.Content, opts => opts.MapFrom(op => op.Content))
-            .ForMember(x => x.Priority, opts => opts.MapFrom(op => op.Priority))
-            .ForMember(x => x.Deadline, opts => opts.MapFrom(op => op.Deadline))
-            .ForMember(x => x.CreatedDate, opts => opts.MapFrom(op => op.CreatedDate))
-            .ForMember(x => x.IsCompleted, opts => opts.MapFrom(op => op.IsCompleted));
+        CreateMap<AddEditTaskDto, Domain.Task>().ConvertUsing<TaskAdapter>();
+        CreateMap<Domain.Task, TaskDto>().ConvertUsing<TaskAdapter>();
     }
 }
