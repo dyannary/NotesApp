58c7951cc065cc94389e7d709706729f11d1a549
Diana <diana.ribac001@gmail.com>
2023-06-07 09:04:51 +0300

    filter notes by descending

---
+++ Notes.Application/Notes/GetAndFilterNotes.cs
@@ -0,0 +1,23 @@
+﻿using Notes.Persistence.Context;
+using System.Net;
+using Notes.DataTransferObjects.Notes;
+using Notes.Domain;
+
+namespace Notes.Application.Notes;
+
+public static class GetAndFilterNotes
+{
+    public static IQueryable<Note> Filter(NotesDbContext notesDbContext, string title)
+    {
+        var notes = notesDbContext.Notes
+            .OrderByDescending(x=>x.CreatedDate)
+            .AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(title))
+        {
+            notes = notes.Where(x => x.Title.ToLower().Contains(title.ToLower()));
+        }
+
+        return notes;
+    }
+}
